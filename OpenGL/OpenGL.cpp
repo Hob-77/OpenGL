29cb74658d274feeffe6ebf2073d9681afce76ba
Hob-77 <hobexe77@gmail.com>
2025-05-12 17:54:25 -0500

    thingy

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -45,9 +45,15 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5, 0.0f,
-		0.0f, 0.5f, 0.0f
+		0.5f, 0.5f, 0.0f,// 0 top right
+		0.5f, -0.5, 0.0f,// 1 bottom right
+	   -0.5f, -0.5f, 0.0f,// 2 bottom left
+	   -0.5f, 0.5f, 0.0f // 3 top left
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3, //first triangle
+		1, 2, 3 // second triangle
 	};
 
 	const char* vertexShaderSource = "#version 460 core\n"
@@ -97,16 +103,24 @@ int main()
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
 	}
 
-	unsigned int VAO;
-	unsigned int VBO;
+	unsigned int VAO, VBO, EBO;
+
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
+	//Bind Vertex Array Object
 	glBindVertexArray(VAO);
 
+	// copy our vertices array in a vertex buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// copy our index array in a element buffer for OpenGL to use
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
 		(void*)0);
 	glEnableVertexAttribArray(0);
@@ -123,7 +137,8 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 
 		//check and call events and swap the buffers
